5c09c6f8968082c8aedae216fee3a8c1b0d913ab
P. Most <PMost@PERA-Software.com>
2016-10-27 17:39:18 +0200

    Renamed cmake scripts

---
+++ cmake/aidkit_options.cmake
@@ -0,0 +1,13 @@
+# Ensure that the library ends up in the same directory as the executable:
+# See also https://cmake.org/cmake/help/latest/variable/CMAKE_RUNTIME_OUTPUT_DIRECTORY_CONFIG.html
+
+set( CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR} )
+set( CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR} )
+set( CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR} )
+
+# Setup aidkit usage
+
+add_subdirectory( ../CppAidKit CppAidKitBuild )
+include_directories( ../CppAidKit/src/ )
+link_directories( CppAidKitBuild )
+
