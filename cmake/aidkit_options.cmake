ea6eed972401136e40086f273f9416a1d9678df3
P. Most <PMost@PERA-Software.com>
2017-01-31 07:34:50 +0100

    Added cmake script for aidkittest

--- cmake/aidkit_options.cmake
+++ cmake/aidkit_options.cmake
@@ -1,13 +1,5 @@
-# Ensure that the library ends up in the same directory as the executable:
-# See also https://cmake.org/cmake/help/latest/variable/CMAKE_RUNTIME_OUTPUT_DIRECTORY_CONFIG.html
-
-set( CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR} )
-set( CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR} )
-set( CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR} )
+include( ${CMAKE_CURRENT_LIST_DIR}/aidkit_common_options.cmake )
 
 # Setup aidkit usage
 
 add_subdirectory( ../CppAidKit CppAidKitBuild )
-include_directories( ../CppAidKit/src/ )
-link_directories( CppAidKitBuild )
-
---
+++ cmake/aidkittest_options.cmake
@@ -0,0 +1,5 @@
+include( ${CMAKE_CURRENT_LIST_DIR}/aidkit_common_options.cmake )
+
+# Setup aidkittest usage
+
+add_subdirectory( ../CppAidKit CppAidKitTestBuild )
