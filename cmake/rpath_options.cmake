b3f700140205ce6893cebccb3ea9b1060a0da67a
P. Most <PMost@PERA-Software.com>
2017-03-20 21:41:12 +0100

    Use RUNPATH instead of RPATH Set RUNPATH to current directory and lib directory

--- cmake/rpath_options.cmake
+++ cmake/rpath_options.cmake
@@ -1,6 +1,27 @@
-# Set the rpath so the resulting target is 'relocatable', meaning the shared libraries in the
-# current directory are being loaded:
+# For inspecting RPATH, RUNPATH etc.:
+# - readelf -d <elffile>
+# - chrpath <program>
 
-set( CMAKE_INSTALL_RPATH "$ORIGIN" )
-set( CMAKE_BUILD_WITH_INSTALL_RPATH ON )
-set( CMAKE_INSTALL_RPATH_USE_LINK_PATH ON )
+# Enable RPATH handling:
+set( CMAKE_SKIP_BUILD_RPATH  FALSE )
+
+# 1) Add the current working directory so shared libraries in the current working directory are used.
+# 2) Add the lib directory from the current working directory because the Qt plugins contain an RUNPATH of
+#    '$ORIGIN/../../lib' where they expect to find the Qt libraries.
+set( CMAKE_INSTALL_RPATH "$ORIGIN;$ORIGIN/lib" )
+
+# "If this variable is set to true then the software is always built with the install path for the
+# RPATH and does not need to be relinked when installed."
+set( CMAKE_BUILD_WITH_INSTALL_RPATH TRUE )
+
+# Append directories in the linker search path and outside the project to the INSTALL_RPATH.
+set( CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE )
+
+# Sets RUNPATH to the same value as RPATH:
+# Note (man ld):
+# "if the linker is being invoked indirectly, via a compiler driver (e.g. gcc) then all the linker
+#  command line options should be prefixed by -Wl"
+set( LINKER_RUNPATH_FLAGS "-Wl,--enable-new-dtags" )
+set( CMAKE_EXE_LINKER_FLAGS    "${LINKER_RUNPATH_FLAGS}" )
+set( CMAKE_SHARED_LINKER_FLAGS "${LINKER_RUNPATH_FLAGS}" )
+
