8434e98f40ba0943d4c87298111a040cfe36f27c
P. Most <PMost@PERA-Software.com>
2017-07-08 22:20:21 +0200

    Added forward declarations and PERAIniSettings

---
+++ src/pera_software/company/qt/ForwardDeclarations.hpp
@@ -0,0 +1,32 @@
+// Copyright 2017 Peter Most, PERA Software Solutions GmbH
+//
+// This file is part of the CppAidKit library.
+//
+// CppAidKit is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CppAidKit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with CppAidKit. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+/// Most of the time only widgets pointers are used and a simple forward declaration would be enough.
+/// The AidKit widgets however are in nested namespaces and it involves a lot of typing for 'manual
+/// forward declarations', so we provide this header.
+
+namespace pera_software { namespace company { namespace qt {
+
+	class PERAAboutDialog;
+	class AboutPERAAction;
+	class PERAApplication;
+	class PERAIniSettings;
+	class PERAMainWindow;
+
+} } }
---
+++ src/pera_software/company/qt/PERAIniSettings.cpp
@@ -0,0 +1,29 @@
+// Copyright 2016 Peter Most, PERA Software Solutions GmbH
+//
+// This file is part of the CppAidKit library.
+//
+// CppAidKit is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CppAidKit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with CppAidKit. If not, see <http://www.gnu.org/licenses/>.
+
+#include "PERAIniSettings.hpp"
+#include <pera_software/company/PERA.hpp>
+#include <pera_software/company/qt/PERAApplication.hpp>
+
+namespace pera_software { namespace company { namespace qt {
+
+PERAIniSettings::PERAIniSettings(const QString &application, QObject *parent)
+	: IniSettings( PERA::NAME, application, parent ){
+}
+
+
+} } }
