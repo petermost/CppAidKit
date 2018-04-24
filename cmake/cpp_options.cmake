46c7d2bd0b5c2af6390d133f37853a637cf0c788
P. Most <PMost@PERA-Software.com>
2018-04-24 18:34:54 +0200

    Removed cmake workaround for requiring C++17

--- cmake/cpp_options.cmake
+++ cmake/cpp_options.cmake
@@ -1,11 +1,16 @@
 # Define the C++ standard:
 
-if ( ${CMAKE_VERSION} VERSION_EQUAL 3.8.0 OR ${CMAKE_VERSION} VERSION_GREATER 3.8.0 )
-	message( FATAL_ERROR "Remove missing cmake C++17 support workaround!")
-	# https://cmake.org/cmake/help/v3.8/prop_tgt/CXX_STANDARD.html
-	set( CMAKE_CXX_STANDARD 17 )
+set( CMAKE_CXX_STANDARD 17 )
 
-	# Don't fall back to an earlier standard:
+# Don't fall back to an earlier standard:
 
-	set( CMAKE_CXX_STANDARD_REQUIRED ON )
-endif()
+set( CMAKE_CXX_STANDARD_REQUIRED ON )
+
+# Warn about C++ constructs whose meaning differs between ISO versions:
+
+add_compile_options(
+	-Wc++11-compat
+	-Wc++14-compat
+	-Wc++17-compat
+	-Wc++1z-compat
+)
