d92996e32935f17430e3d03f1eac6a14d18c3ed3
P. Most <PMost@PERA-Software.com>
2018-07-02 21:30:04 +0200

    Removed C++17 workaround, Added more warning flags

--- cmake/cpp_options.cmake
+++ cmake/cpp_options.cmake
@@ -5,12 +5,3 @@ set( CMAKE_CXX_STANDARD 17 )
 # Don't fall back to an earlier standard:
 
 set( CMAKE_CXX_STANDARD_REQUIRED ON )
-
-# Warn about C++ constructs whose meaning differs between ISO versions:
-
-add_compile_options(
-	-Wc++11-compat
-	-Wc++14-compat
-	-Wc++17-compat
-	-Wc++1z-compat
-)
--- cmake/gcc_options.cmake
+++ cmake/gcc_options.cmake
@@ -10,17 +10,52 @@ endif()
 # Add GCC specific switches:
 
 add_compile_options(
-	-pipe                       # Avoid temporary files, speeding up builds.
 	-Wall -Wextra -Wpedantic    # Enable all warnings
+	-Wimplicit-fallthrough
 	-Wmissing-include-dirs      # Warn if a user-supplied include directory does not exist.
-	-Wundef                     # Warn if an undefined identifier is evaluated in an #if directive.
-	-Wcast-align                # Warn whenever a pointer is cast such that the required alignment of the target is increased.
-	-Wold-style-cast            # Warn if an old-style (C-style) cast to a non-void type is used within a C++ program.
+	-Wswitch-default
+	# -Wswitch-enum
+	-Wunused-parameter
+	-Wunused-const-variable
+	-Wuninitialized
+	-Wunknown-pragmas
+	-Wsuggest-final-types
+	-Wsuggest-final-methods
 	-Wsuggest-override          # Warn about overriding virtual functions that are not marked with the override keyword.
+	-Walloc-zero
+	-Walloca
+	-Wduplicated-branches
+	-Wduplicated-cond
+	# -Wsystem-headers
+	-Wtrampolines
+	-Wfloat-equal
+	# -Wundef                     # Warn if an undefined identifier is evaluated in an #if directive.
+	-Wunused-macros
+	-Wcast-qual
+	-Wcast-align                # Warn whenever a pointer is cast such that the required alignment of the target is increased.
+	-Wconditionally-supported
+	-Wzero-as-null-pointer-constant
+	-Wdangling-else
+	-Wdate-time
+	-Wuseless-cast
+	-Wsign-conversion
+	-Wfloat-conversion
+	-Wlogical-op
+	-Wmissing-field-initializers
+	-Wpacked
+	-Wpadded
+	-Winvalid-pch
+	-Wabi
+	-Wctor-dtor-privacy
+	-Wnoexcept
+	-Wnoexcept-type
+	-Wnon-virtual-dtor
+	-Wold-style-cast            # Warn if an old-style (C-style) cast to a non-void type is used within a C++ program.
+	-Woverloaded-virtual
 
+	-pipe                       # Avoid temporary files, speeding up builds.
 	-fvisibility=hidden         # Make symbols hidden by default i.e. must be explicitly exported
 	-fdiagnostics-show-option   # Show which compiler switch caused a warning
-
 	-pthread                    # Adds support for multithreading with the pthreads library. This option sets flags for both the preprocessor and linker
 )
 
