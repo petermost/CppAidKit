bb460e5f1b05404a737c5e0c8ac034c8071fc2db
P. Most <PMost@PERA-Software.com>
2019-01-06 14:17:44 +0100

    Create 'compile_commands.json', Improve CMAKE_BUILD_TYPE handling

--- cmake/cmake_options.cmake
+++ cmake/cmake_options.cmake
@@ -1,16 +1,12 @@
 # Contains general options for cmake
 
 set( CMAKE_WARN_DEPRECATED ON )
+set( CMAKE_VERBOSE_MAKEFILE TRUE )
+set( CMAKE_EXPORT_COMPILE_COMMANDS ON )
 
-if ( NOT CMAKE_BUILD_TYPE )
+if ( NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES )
 	message( "Enabling debug build" )
-	set( CMAKE_BUILD_TYPE Debug CACHE STRING "Select build type")
+	set( CMAKE_BUILD_TYPE Debug CACHE STRING "Select build type" FORCE)
 	# Present a combobox in the cmake-gui (https://blog.kitware.com/cmake-and-the-default-build-type/):
 	set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
 endif()
-
-if ( NOT CMAKE_VERBOSE_MAKEFILE )
-	message( "Enabling verbose makefile" )
-	set( CMAKE_VERBOSE_MAKEFILE TRUE )
-endif()
-
