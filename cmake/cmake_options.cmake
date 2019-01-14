723ae4b054b6ebf5f0b5f17417396c40408f1deb
Peter Most <none>
2019-01-14 16:24:12 +0100

    Don't specify the library build type explicitly but use the BUILD_SHARED_LIB variable

--- cmake/cmake_options.cmake
+++ cmake/cmake_options.cmake
@@ -4,9 +4,12 @@ set( CMAKE_WARN_DEPRECATED ON )
 set( CMAKE_VERBOSE_MAKEFILE TRUE )
 set( CMAKE_EXPORT_COMPILE_COMMANDS ON )
 
+option( BUILD_SHARED_LIBS "Build shared library" ON )
+
 if ( NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES )
 	message( "Enabling debug build" )
 	set( CMAKE_BUILD_TYPE Debug CACHE STRING "Select build type" FORCE)
 	# Present a combobox in the cmake-gui (https://blog.kitware.com/cmake-and-the-default-build-type/):
 	set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
 endif()
+
