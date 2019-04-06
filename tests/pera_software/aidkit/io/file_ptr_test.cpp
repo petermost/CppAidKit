ebc69671cb514b1da1e50f1bca7831deca91afe3
P. Most <PMost@PERA-Software.com>
2019-04-06 15:41:00 +0200

    Added file_ptr alias and tests for unique_ptr<FILE>

---
+++ tests/pera_software/aidkit/io/file_ptr_test.cpp
@@ -0,0 +1,31 @@
+// Copyright 2019 Peter Most, PERA Software Solutions GmbH
+//
+// This file is part of the CppAidKit library.
+//
+// CppAidKit is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CppAidKit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with CppAidKit. If not, see <http://www.gnu.org/licenses/>.
+
+#include "file_ptr_test.hpp"
+#include <pera_software/aidkit/io/file_ptr.hpp>
+#include <QTest>
+
+namespace pera_software::aidkit::io {
+
+using namespace std;
+
+void FilePtrTest::testConstructor() {
+	file_ptr file(fopen("", ""));
+	fgetc(file.get());
+}
+
+}
