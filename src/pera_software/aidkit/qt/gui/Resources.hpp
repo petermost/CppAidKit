39784c6be9ce8a58001321c9f9c85135bf48b248
P. Most <PMost@PERA-Software.com>
2019-04-07 22:07:16 +0200

    Reverted signature change

--- src/pera_software/aidkit/qt/gui/Resources.hpp
+++ src/pera_software/aidkit/qt/gui/Resources.hpp
@@ -24,7 +24,7 @@ class QString;
 
 namespace pera_software::aidkit::qt {
 
-	QIcon AIDKIT_API loadIcon(const QString &iconName);
+	AIDKIT_API QIcon loadIcon(const QString &iconName);
 
 	class AIDKIT_API Resources {
 		public:
