d34a3d1835181cdfe20c42d6d69795ff26b5b9bf
Peter Most <pmost@pera-software.com>
2019-04-09 16:21:04 +0200

    Added warning about possible linker problems

--- cmake/output_options.cmake
+++ cmake/output_options.cmake
@@ -5,6 +5,13 @@
 # https://cmake.org/cmake/help/latest/manual/cmake-buildsystem.7.html#library-output-artifacts
 # https://cmake.org/cmake/help/latest/manual/cmake-buildsystem.7.html#archive-output-artifacts
 
-set( CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR} )
-set( CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR} )
-set( CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR} )
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
+
+# Attention!
+# If the binary directory already contains a directory with the same name as the resulting build 
+# target (executable) then the linker will complain: 'ld: cannot open output file .: Is a directory'
+# This will most likely only happen under Linux, because executables don't get an .exe extension.
+# https://stackoverflow.com/questions/38168725/cmake-trying-to-link-directory-as-an-executable-if-their-names-match
+
