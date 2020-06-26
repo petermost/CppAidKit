2214dd379b999800fbdfcb08c60dbf9b5066a6e4
P. Most <pmost@pera-software.com>
2020-06-26 14:41:17 +0200

    Moved non-target based cmake options to all_options.cmake

--- cmake/all_options.cmake
+++ cmake/all_options.cmake
@@ -1,5 +1,4 @@
 include(${CMAKE_CURRENT_LIST_DIR}/report.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/cmake_options.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/cpp_options.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/library_options.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/output_options.cmake)
@@ -11,23 +10,14 @@ include(${CMAKE_CURRENT_LIST_DIR}/msvc_options.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/libstdcxx_options.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/libcxx_options.cmake)
 
-
-set(BUILD_SHARED_LIBS      ON CACHE BOOL "Build shared library" FORCE)
-set(CMAKE_VERBOSE_MAKEFILE ON CACHE BOOL "Verbose Makefile" FORCE)
-
-report("CMAKE_VERSION: '${CMAKE_VERSION}'")
-
-# See https://cmake.org/cmake/help/latest/variable/CMAKE_LANG_COMPILER_ID.html for a list of
-# compiler ids.
-report("CMAKE_CXX_COMPILER_ID: '${CMAKE_CXX_COMPILER_ID}', CMAKE_CXX_COMPILER_VERSION: '${CMAKE_CXX_COMPILER_VERSION}'")
-
 function(set_default_target_options targetName)
-	set_default_cmake_options()
 	set_default_cpp_target_options(${targetName})
 	set_default_rpath_target_options(${targetName})
 	set_default_output_target_options(${targetName})
 	set_default_library_target_options(${targetName})
 
+	# See https://cmake.org/cmake/help/latest/variable/CMAKE_LANG_COMPILER_ID.html for a list of # compiler ids.
+	# report("CMAKE_CXX_COMPILER_ID: '${CMAKE_CXX_COMPILER_ID}', CMAKE_CXX_COMPILER_VERSION: '${CMAKE_CXX_COMPILER_VERSION}'")
 	if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
 		if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
 			set_default_gcc_target_options(${targetName})
