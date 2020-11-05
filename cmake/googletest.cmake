9a4ad723e7861a5d216c82fe3e0e6ec6cfbe41b2
Peter Most <peter.most.ext@zeiss.com>
2020-11-05 17:22:50 +0100

    Fetch GoogleTest if it is not installed

--- cmake/googletest.cmake
+++ cmake/googletest.cmake
@@ -7,5 +7,19 @@ function(add_googletest_subdirectory)
 		set(GTEST_FOUND TRUE PARENT_SCOPE)
 		add_library(GTest::GTest ALIAS gtest)
 		add_library(GTest::Main ALIAS gtest_main)
+	else()
+		include(FetchContent)
+		FetchContent_Declare(googletest
+			GIT_REPOSITORY https://github.com/google/googletest.git
+			GIT_TAG        release-1.10.0
+		)
+		FetchContent_MakeAvailable(googletest)
+		if (NOT TARGET gtest AND NOT TARGET gtest_main)
+			add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR})
+		endif()
+		# Create the same targets/variables which FindGTest would have done:
+		set(GTEST_FOUND TRUE PARENT_SCOPE)
+		add_library(GTest::GTest ALIAS gtest)
+		add_library(GTest::Main ALIAS gtest_main)
 	endif()
 endfunction()
