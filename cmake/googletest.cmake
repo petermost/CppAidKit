9a05695e031cd29c1196f18e570ae41fdf2c4328
P. Most <pmost@pera-software.com>
2020-11-17 13:03:12 +0100

    Simplified helper function add_googletest_subdirectory

--- cmake/googletest.cmake
+++ cmake/googletest.cmake
@@ -1,25 +1,23 @@
 function(add_googletest_subdirectory)
-	if (EXISTS /usr/src/googletest)
-		if (NOT TARGET gtest AND NOT TARGET gtest_main)
+	if (NOT TARGET gtest AND NOT TARGET gtest_main)
+		if (EXISTS /usr/src/googletest)
 			add_subdirectory(/usr/src/googletest googletest)
-		endif()
-		# Create the same targets/variables which FindGTest would have done:
-		set(GTEST_FOUND TRUE PARENT_SCOPE)
-		add_library(GTest::GTest ALIAS gtest)
-		add_library(GTest::Main ALIAS gtest_main)
-	else()
-		include(FetchContent)
-		FetchContent_Declare(googletest
-			GIT_REPOSITORY https://github.com/google/googletest.git
-			GIT_TAG        release-1.10.0
-		)
-		FetchContent_MakeAvailable(googletest)
-		if (NOT TARGET gtest AND NOT TARGET gtest_main)
+		else()
+			include(FetchContent)
+			FetchContent_Declare(googletest
+				GIT_REPOSITORY https://github.com/google/googletest.git
+				GIT_TAG        release-1.10.0
+			)
+			FetchContent_MakeAvailable(googletest)
 			add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR})
 		endif()
 		# Create the same targets/variables which FindGTest would have done:
 		set(GTEST_FOUND TRUE PARENT_SCOPE)
 		add_library(GTest::GTest ALIAS gtest)
 		add_library(GTest::Main ALIAS gtest_main)
+
+		# Create the targets in 'our' prefered directory:
+		set_default_output_target_options(gtest)
+		set_default_output_target_options(gtest_main)
 	endif()
 endfunction()
